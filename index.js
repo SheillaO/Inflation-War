3e254708cd36de4fc2b20a0b703c6751a2b982c3
SheillaO <sheilla.alusa@strathmore.edu>
2026-06-17 22:24:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let deckId;
 let computerScore = 0;
 let myScore = 0;
 
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+const header = document.getElementById("header");
+const remainingText = document.getElementById("remaining");
+const computerScoreEl = document.getElementById("computer-score");
+const myScoreEl = document.getElementById("my-score");
